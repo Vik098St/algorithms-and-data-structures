278c4b982b36e257a5fa2843f40a707ab9baedcf
Vik098St <62994846+Vik098St@users.noreply.github.com>
2021-07-16 22:56:25 +0300

    Lab3 foolder uploaded

---
+++ Lab3_Sem4/cmake-build-debug/CMakeFiles/Lab3_Sem4.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Lab3_Sem4.dir/CW.cpp.obj"
+  "Lab3_Sem4.exe"
+  "Lab3_Sem4.exe.manifest"
+  "Lab3_Sem4.pdb"
+  "libLab3_Sem4.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Lab3_Sem4.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
